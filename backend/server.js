d5415435194278db50b1b9c4147fdabe701c367d
Krvien16 <kriven.govender07@gmail.com>
2025-10-16 23:30:13 +0200

    fixed scripts for render.com

--- backend/server.js
+++ backend/server.js
@@ -40,6 +40,15 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
   });
 }
+// if(process.env.NODE_ENV ==='production'){
+//   app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+//   app.get("*",(req,res)=>{
+//     res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+//   })
+
+
+// }
 
 
 
