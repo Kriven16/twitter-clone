6fd2f83a8d60c45c69bc3a9a7be807d9ef01c47d
Krvien16 <kriven.govender07@gmail.com>
2025-10-16 23:02:22 +0200

    added deployment for render.com

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express, { urlencoded } from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import {v2 as cloudinary } from "cloudinary"
+import path from "path"
 
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -19,6 +20,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000
+const __dirname = path.resolve()
 
 //middlerware
 app.use(express.json())//to parse req.body
@@ -30,6 +32,15 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/post",postRoutes)
 app.use("/api/notifications",notificationRoutes)
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend", "dist")));
+
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
 
 
 
